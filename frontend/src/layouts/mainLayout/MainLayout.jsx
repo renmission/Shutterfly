c2fe45ca4629af3de0eea68b563e1be942aa1c5f
renmission <missionrenjr@gmail.com>
2024-07-31 20:22:59 +0800

    updated footer

--- frontend/src/layouts/mainLayout/MainLayout.jsx
+++ frontend/src/layouts/mainLayout/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom"
 import NavigationHeader from "../../components/Navigation/NavigationHeader"
-import Footer from "../../components/Footer/Footer"
 
 const MainLayout = () => {
   return (
     <>
         <NavigationHeader />
         <Outlet />
-        <Footer />
     </>
   )
 }
